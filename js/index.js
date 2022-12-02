359b58724d56292e8c76adc42f96d326a5ce0981
DmytroB <dmitrijbaraban@gmail.com>
2022-12-02 18:08:38 +0200

    mobile header complite

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// burger menu
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let logo = document.querySelector('.logo')
+let header = document.getElementsByTagName('header')[0];
+
+menuBtn.addEventListener('click', function(){
+menuBtn.classList.toggle('active');
+menu.classList.toggle('active');
+logo.classList.toggle('active');
+header.classList.toggle('active')
+})
+// 
