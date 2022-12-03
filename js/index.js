5337cacff675d419d8a1134f8a8a99eae9b9c356
DmytroB <dmitrijbaraban@gmail.com>
2022-12-03 14:39:26 +0200

    some changes

--- js/index.js
+++ js/index.js
@@ -10,4 +10,16 @@ menu.classList.toggle('active');
 logo.classList.toggle('active');
 header.classList.toggle('active')
 })
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop; // Получаем положение скролла
+    if(scrolled !== 0){
+      // Если прокрутка есть, то делаем блок прозрачным
+      document.querySelector('.menu-btn span').style.background = '#005BBB';
+    }else{
+      // Если нет, то делаем его полностью видимым
+      document.querySelector('.menu-btn span').style.background = '#FFFFFF';
+    };
+  };
+
 // 
