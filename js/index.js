c835fdb3b5c836f32ddb3ced8852da17f701517c
DmytroB <dmitrijbaraban@gmail.com>
2022-12-03 22:14:01 +0200

    complite scroll

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // burger menu
+
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let logo = document.querySelector('.logo')
@@ -11,15 +12,14 @@ logo.classList.toggle('active');
 header.classList.toggle('active')
 })
 
-window.onscroll = function() {
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop; // Получаем положение скролла
-    if(scrolled !== 0){
-      // Если прокрутка есть, то делаем блок прозрачным
-      document.querySelector('.menu-btn span').style.background = '#005BBB';
-    }else{
-      // Если нет, то делаем его полностью видимым
-      document.querySelector('.menu-btn span').style.background = '#FFFFFF';
-    };
-  };
+// scroll
 
-// 
+window.onscroll = function() {
+  const scrolled = window.pageYOffset || document.documentElement.scrollTop; // Отримуємо положення скролу
+    if(scrolled !== 0){
+      $('.menu-btn span').css('background', '#005BBB');
+  }
+    else{
+      $('.menu-btn span').css('background', '#FFFFFF');
+  }
+};
