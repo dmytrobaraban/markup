2afb75722471804789dd3c5f275c8c090ae0692d
DmytroB <dmitrijbaraban@gmail.com>
2022-12-04 01:56:36 +0200

    fuck scrolling

--- js/index.js
+++ js/index.js
@@ -4,22 +4,12 @@ let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let logo = document.querySelector('.logo')
 let header = document.getElementsByTagName('header')[0];
+let bodyHidden = document.getElementsByTagName('body')[0];
 
 menuBtn.addEventListener('click', function(){
 menuBtn.classList.toggle('active');
 menu.classList.toggle('active');
 logo.classList.toggle('active');
-header.classList.toggle('active')
+header.classList.toggle('active');
+bodyHidden.classList.toggle('active');
 })
-
-// scroll
-
-window.onscroll = function() {
-  const scrolled = window.pageYOffset || document.documentElement.scrollTop; // Отримуємо положення скролу
-    if(scrolled !== 0){
-      $('.menu-btn span').css('background', '#005BBB');
-  }
-    else{
-      $('.menu-btn span').css('background', '#FFFFFF');
-  }
-};
